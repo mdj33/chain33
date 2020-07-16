0e13893a3a80805d2b0c8721cd29733006a55293
mdj33 <mdj@disanbo.com>
2020-07-16 19:36:32 +0800

    peerinfo

--- system/p2p/dht/protocol/peer/peerinfo.go
+++ system/p2p/dht/protocol/peer/peerinfo.go
@@ -71,23 +71,27 @@ func (p *peerInfoProtol) fetchPeersInfo() {
 func (p *peerInfoProtol) getLoacalPeerInfo() *types.P2PPeerInfo {
 	var peerinfo types.P2PPeerInfo
 
-	//resp, err := p.QueryMempool(types.EventGetMempoolSize, nil)
-	//if err != nil {
-	//	log.Error("getlocalPeerInfo", "sendToMempool", err)
-	//	return nil
-	//}
-	//
-	//meminfo := resp.(*types.MempoolSize)
-	//peerinfo.MempoolSize = int32(meminfo.GetSize())
+	log.Info("getLoacalPeerInfo enter")
+	resp, err := p.QueryMempool(types.EventGetMempoolSize, nil)
+	if err != nil {
+		log.Error("getlocalPeerInfo", "sendToMempool", err)
+		return nil
+	}
 
-	//resp, err = p.QueryBlockChain(types.EventGetLastHeader, nil)
-	//if err != nil {
-	//	log.Error("getlocalPeerInfo", "sendToBlockChain", err)
-	//	return nil
-	//}
-	//
-	//header := resp.(*types.Header)
-	//peerinfo.Header = header
+	log.Info("getLoacalPeerInfo, mempool over")
+
+	meminfo := resp.(*types.MempoolSize)
+	peerinfo.MempoolSize = int32(meminfo.GetSize())
+
+	resp, err = p.QueryBlockChain(types.EventGetLastHeader, nil)
+	if err != nil {
+		log.Error("getlocalPeerInfo", "sendToBlockChain", err)
+		return nil
+	}
+	log.Info("getLoacalPeerInfo, blockchain over")
+
+	header := resp.(*types.Header)
+	peerinfo.Header = header
 	peerinfo.Name = p.Host.ID().Pretty()
 	splites := strings.Split(p.Host.Addrs()[0].String(), "/")
 	port, _ := strconv.Atoi(splites[len(splites)-1])
